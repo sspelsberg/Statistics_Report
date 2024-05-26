f0bbf169b601ea58056f854c38aa9dfa4eb0d784
sophie <sspelsbe@students.uni-mainz.de>
2024-05-26 16:07:13 +0200

    continued cluster comparison

--- R/cluster_comparison.R
+++ R/cluster_comparison.R
@@ -37,21 +37,51 @@ station_names <- readRDS("data_clean/station_names.rds")
 variables <- readRDS("data_clean/variables.rds")
 
 
-# look at precip data per cluster ----------------------
+# Data preparation -----------------------------
 
-# raw precip data from pca
-data_precip <- data_selected |> filter(stn != "JUN")
+# add yearly precip to raw precip data dataframe
+data_precip <- data_selected |> # raw precip data from pca
+  dplyr::mutate(year = year(time), # add year and month column
+                month = month(time)) |>
+  dplyr::filter(year != 2024) |>
+  dplyr::filter(stn != "JUN") |> # exclude Jungfraujoch (doesn't contain precip data)
+  dplyr::group_by(stn, year) |>
+  dplyr::mutate(precip_annual = sum(rre150m0)) |> # add column with annual precipitation per station
+  dplyr::ungroup() |>
+  dplyr::left_join(station_metadata_cluster |>
+                     select(stn, cluster_pca_h)) |> # add cluster info
+  dplyr::group_by(cluster_pca_h, year, month) |>
+  dplyr::mutate(precip_cluster_mean_monthly = mean(rre150m0)) |> # compute monthly cluster mean (contains cluster timeseries)
+  dplyr::ungroup()
 
-# fill cluster column based on pca_hierarchic clusters from metadata dataframe
-data_precip$cluster <- station_metadata_cluster[match(data_precip$stn, station_metadata_cluster$stn), 14]
+# create new df with only the yearly values
+data_precip_annual <- data_precip |>
+  dplyr::select(stn, year, precip_annual, cluster_pca_h) |>
+  dplyr::distinct() |> # keep only unique rows
+  dplyr::group_by(cluster_pca_h, year) |>
+  dplyr::mutate(precip_cluster_mean = mean(precip_annual)) |> # add column with annual cluster mean values
+  dplyr::ungroup()
 
-# select all data from january
+
+
+# Boxplots for cluster comparison ----------------------
+
+# plot boxplot with annual data by cluster (datapoints = single stations)
+data_precip_annual |>
+  ggplot(aes(x = cluster_pca_h, y = precip_annual)) +
+  geom_boxplot() # or geom_violin()
+
+# plot boxplot with annual data by cluster (datapoints = annual cluster means)
+data_precip_annual |>
+  ggplot(aes(x = cluster_pca_h, y = precip_cluster_mean)) +
+  geom_boxplot() # or geom_violin()
+
+
+# boxplots for every month with individual station data
 data_precip |>
-  filter(month(time) == 6) |>
-
-  # plot boxplot by cluster
-  ggplot(aes(x = cluster, y = rre150m0)) +
-    geom_boxplot()
+  ggplot(aes(x = cluster_pca_h, y = rre150m0)) +
+  geom_boxplot() +
+  facet_wrap(~month(time))
 
 
 # compare data between stations for all clusters
