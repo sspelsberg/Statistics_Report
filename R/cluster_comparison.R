a8c322149e6cca2e24cdcb953a1e0925947feffa
sophie <sspelsbe@students.uni-mainz.de>
2024-05-26 21:11:19 +0200

    added climate diagram code for comparing the clusters

--- R/cluster_comparison.R
+++ R/cluster_comparison.R
@@ -28,9 +28,11 @@ data <- read.csv("data_clean/data.csv") |>
   mutate(time = lubridate::ym(time))
 data_stats <- read.csv("data_clean/data_stats.csv")
 data_precip <- read.csv("data_clean/data_precip.csv") |>
-  mutate(cluster_pca_h = as.factor(cluster_pca_h))
+  mutate(cluster_pca_h = as.factor(cluster_pca_h),
+         time = lubridate::ymd(time))
 data_precip_annual <- read.csv("data_clean/data_precip_annual.csv") |>
   mutate(cluster_pca_h = as.factor(cluster_pca_h))
+data_clim_diagram <- read.csv("data_clean/data_clim_diagram.csv")
 
 # read metadata
 station_metadata <- read.csv("data_clean/station_metadata.csv")
@@ -65,6 +67,28 @@ data_precip_annual <- data_precip |>
   dplyr::group_by(cluster_pca_h, year) |>
   dplyr::mutate(precip_cluster_mean = mean(precip_annual)) |> # add column with annual cluster mean values
   dplyr::ungroup()
+
+# create df for climate diagram per cluster (mean monthly precip and temp)
+data_clim_diagram <- data_precip |>
+  dplyr::left_join(data |> select(stn, time, tre200m0)) |>
+  dplyr::group_by(cluster_pca_h, month) |>
+  dplyr::summarise(mean_precip = mean(rre150m0),
+                   mean_temp = mean(tre200m0))
+
+
+# climate diagrams for the 4 clusters -----------------------
+
+data_clim_diagram |>
+  ggplot(aes(x = month, y = mean_temp)) +
+  geom_hline(yintercept = 0, linewidth = 0.3) + # black line at 0
+  geom_col(aes(y = mean_precip * 0.5), fill = "#2b94c4", alpha = 0.5) + # add precip bars and set transparency to 0.5
+  geom_line(color = "#d91f00", linewidth = 0.8) + # add temperature data
+  facet_wrap(~cluster_pca_h) + # split by cluster
+  scale_y_continuous("Mean Temperature [°C]",
+                     sec.axis = sec_axis(~ . * 2, name = "Mean Precipitation [mm]")) + # add the second y axis
+  scale_x_continuous(breaks = c(1:12), expand = c(0.01,0.01)) + # modify x ticks and remove space left and right
+  theme(panel.grid.major = element_blank()) # remove parts of the grid
+
 
 
 
@@ -105,6 +129,7 @@ data_precip_annual |>
 
 write_csv(data_precip, "data_clean/data_precip.csv")
 write_csv(data_precip_annual, "data_clean/data_precip_annual.csv")
+write_csv(data_clim_diagram, "data_clean/data_clim_diagram.csv")
 
 
 
